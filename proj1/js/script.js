e712f7de555bc5d8370b5261fbcfa9a231ce314c
ratov <ratov@mail.ru>
2019-04-06 17:50:24 +0300

    This is our first commit

---
+++ proj1/js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let isCh = true, isCl = false;
+console.log(isCh || isCl);
+
+
+// let incr = 10,
+// 		decr = 10;
+// incr++;
+// decr--;
+// console.log(++incr);
+// console.log(--decr);
+// console.log('2' === 2);
+
+// let strN = 3 + "4";
+// console.log("arr " + " - object");
+// console.log(typeof(null));
+
+/*Типы данных*/
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
