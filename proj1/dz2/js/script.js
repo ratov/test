b1a14ace332aaed52f0bbcd00125104baeedbf80
ratov <ratov@mail.ru>
2019-04-17 19:57:17 +0300

    17.04.2019 19:57

--- proj1/dz2/js/script.js
+++ proj1/dz2/js/script.js
@@ -1,7 +1,14 @@
 let menuItem = document.querySelectorAll('.menu .menu-item'),
     menu = document.querySelector('menu');
-
     menuItem[1].innerHTML = 'Второй пункт';
     menuItem[2].innerHTML = 'Третий пункт';
 
-document.body.style.backgroundImage = 'url(../img/apple_true.jpg)';
+let li = document.createElement('li'),
+    txt = document.createTextNode('Пятый пункт');
+
+// let appLi = li.classList.add('menu-item');
+// li.classList.add('menu-item');
+li.appendChild(txt);
+menu.parentNode.appendChild(li);
+
+// document.body.style.backgroundImage = 'url(../img/apple_true.jpg)';
